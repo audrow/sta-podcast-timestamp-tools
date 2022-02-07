f8bb3ba09e668930ff0fd7848ea1bfc2bd49be22
Audrow Nash <audrow@hey.com>
2022-02-07 08:23:20 -0800

    Initial commit
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,47 @@
-import Person from './__types__/Person'
+import type {Timestamp} from '@sta-podcast/types'
 
-export function whoIsYounger(personA: Person, personB: Person): Person {
-  return personA.age < personB.age ? personA : personB
+export type {Timestamp}
+
+export function toTimestamp(s: string): Timestamp {
+  const match = s.match(/^(\d+):(\d{2}):(\d{2})(?:[,|.]\d+)?$/)
+  if (!match) {
+    throw new Error(`"${s}" is not a valid duration`)
+  }
+  const duration = {
+    hours: parseInt(match[1]),
+    minutes: parseInt(match[2]),
+    seconds: parseInt(match[3]),
+  }
+  if (duration.minutes > 59 || duration.seconds > 59) {
+    throw new Error(`"${s}" is not a valid duration`)
+  }
+  return duration
 }
 
-if (typeof require !== 'undefined' && require.main === module) {
-  console.log(whoIsYounger({name: 'John', age: 20}, {name: 'Jane', age: 30}))
+export function toTimestampString(duration: Timestamp): string {
+  return `${duration.hours}:${duration.minutes
+    .toString()
+    .padStart(2, '0')}:${duration.seconds.toString().padStart(2, '0')}`
+}
+
+export function sortTimestamps(a: Timestamp, b: Timestamp) {
+  if (a.hours > b.hours) {
+    return 1
+  } else if (a.hours < b.hours) {
+    return -1
+  }
+
+  if (a.minutes > b.minutes) {
+    return 1
+  } else if (a.minutes < b.minutes) {
+    return -1
+  }
+
+  if (a.seconds > b.seconds) {
+    return 1
+  } else if (a.seconds < b.seconds) {
+    return -1
+  }
+
+  return 0
 }
